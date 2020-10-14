19529e1b7e3cafec3fc2e18c817f71c9c1a05ee5
xitongsys <xitongsys@gmail.com>
2020-10-14 08:38:26 +0800

    socket

--- err.go
+++ err.go
@@ -7,3 +7,4 @@ import (
 var ERR_READ_FROM_WRITE_TUBE = fmt.Errorf("read from a WriterTube")
 var ERR_WRITE_TO_READERTUBE = fmt.Errorf("write to a ReaderTube")
 var ERR_TUBE_IS_FULL = fmt.Errorf("tube is full")
+var ERR_OPERATION_NOT_SUPPORT = fmt.Errorf("operation not supported")
---
+++ socket.go
@@ -0,0 +1,108 @@
+package tube
+
+import (
+	"io"
+	"net"
+)
+
+type SocketTube struct {
+	InternalTube
+	Address	string
+
+	//for writer
+	closed	bool
+	tcpListener *net.TCPListener
+
+	//for reader
+	tcpConn net.Conn
+	
+}
+
+func NewSocketTubeWriter(capacity int, address string) (*SocketTube, error) {
+	if _, err := net.ResolveTCPAddr("tcp", address); err != nil {
+		return nil, err
+	}
+
+	st := & SocketTube {
+		InternalTube: *NewInternalTubeWriter(capacity),
+		Address: address,
+		closed: false,
+
+	}
+
+	return st, nil
+}
+
+func NewSocketTubeReader(capacity int, address string) (*SocketTube, error) {
+	st := & SocketTube {
+		InternalTube: *NewInternalTubeWriter(capacity),
+		Address: address,
+		closed: false,
+	}
+
+	return st, nil
+}
+
+func (st *SocketTube) Start() error {
+	if st.Role() == READER {
+		return st.startReader()
+	}
+
+	return st.startWriter()
+}
+
+func (st *SocketTube) startReader() (err error) {
+	st.tcpConn, err = net.Dial("tcp", st.Address)
+	go func(){
+		for {
+			if _, err := io.Copy(st, st.tcpConn); err != nil {
+				return
+			}
+		}
+	}()
+	return err
+}
+
+func (st *SocketTube) startWriter() error {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", st.Address)
+	if err != nil {
+		return err
+	}
+
+	st.tcpListener, err = net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		return nil
+	}
+
+	go func() {
+		for {
+			conn, err := st.tcpListener.AcceptTCP()
+			if err != nil && st.closed {
+				return
+			}
+
+			go func(){
+				defer func(){
+					conn.Close()
+				}()
+
+				if _, err := io.Copy(conn, st); err != nil {
+					return
+				}
+			}()
+
+		}
+	}()
+
+	return nil
+}
+
+func (st *SocketTube) Close() error {
+	st.InternalTube.Close()
+
+	if st.Role() == READER {
+		return st.tcpConn.Close()
+	} else {
+		return st.tcpListener.Close()
+	}
+}
